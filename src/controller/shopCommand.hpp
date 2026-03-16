76e0087d6cf913a76247ecaf219ee03ebaa58c39
melisa <melisa.verdu@mi.unc.edu.ar>
2026-03-16 12:57:25 -0300

    feat: add shopCommand header

---
+++ src/controller/shopCommand.hpp
@@ -0,0 +1,8 @@
+#ifndef _SHOP_COMMAND_HPP
+#define _SHOP_COMMAND_HPP
+//evita inclusiones múltiples
+
+#include "controller/command.hpp" // incluyo clase base Command, para heredar
+
+#include <string>
+#include <vector>
