dbaff41810279127a597d64133374d190cae807d
Ángela Marioni <angela.marioni@mi.unc.edu.ar>
2026-03-18 23:02:57 -0300

    feat: add TimeCommand and add to commandRegistry.cpp

--- src/controller/timeCommand.cpp
+++ src/controller/timeCommand.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "timeCommand.hpp"
+#include "common/types.hpp"
+#include "model/gameModel.hpp"
+#include <iostream>
+namespace CyberpunkCba
+{
+
+    std::string TimeCommand::name() const
+    {
+        return "time";
+    }
+    std::string TimeCommand::description() const
+    {
+        return "Devuelve turno y hora.";
+    }
+    std::string TimeCommand::category() const
+    {
+        return "sistema";
+    }
+
+    void TimeCommand::execute(GameModel& model)
+    {
+        if (model.currentHour() == 0 && model.currentMinute() == 0)
+        {
+            std::cout << "Nuevo día de simulación iniciado.";
+        }
+        else
+        {
+            std::cout << " Hora: " << model.currentHour() << ":" << model.currentMinute()
+                      << " | Turno: " << timeOfDayToString(timeOfDayFromHour(model.currentHour()))
+                      << " | Tiempo restante hasta próximo turno: "
+                      << minutesUntilNextTurn(model.currentHour(), model.currentMinute()) << " minutos." << std::endl;
+        }
+    }
+} // namespace CyberpunkCba
--- src/controller/timeCommand.hpp
+++ src/controller/timeCommand.hpp
@@ -4,12 +4,27 @@
 
 #ifndef CYBERPUNK_CORDOBA_2077_TIMECOMMAND_HPP
 #define CYBERPUNK_CORDOBA_2077_TIMECOMMAND_HPP
+#include "controller/command.hpp"
+
 /*
  * @class TimeCommand
  * @brief
+ * @author lorem ipsum Team
  */
-class TimeCommand
+namespace CyberpunkCba
 {
-};
+
+    class TimeCommand final : public Command
+    {
+    public:
+        TimeCommand() = default;
+        ~TimeCommand() override = default;
+        void execute(GameModel& model) override;
+        [[nodiscard]] std::string name() const override;
+        [[nodiscard]] std::string description() const override;
+        [[nodiscard]] std::string category() const override;
+    };
+
+} // namespace CyberpunkCba
 
 #endif // CYBERPUNK_CORDOBA_2077_TIMECOMMAND_HPP
