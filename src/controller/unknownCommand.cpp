1a81ddcfc828e63f5e23192f2445cbda9a7ab232
Giuliana Bettera L. <giuliana.bettera@mi.unc.edu.ar>
2026-03-20 00:52:01 -0300

    no m maten pls, complete el constructor

--- src/controller/unknownCommand.cpp
+++ src/controller/unknownCommand.cpp
@@ -1,5 +1,5 @@
 #include "unknownCommand.hpp"
-
+#include "controller/commandRegistry.hpp" // para usar CommandRegistry en el constructor y en execute().
 #include <iostream>
 
 namespace CyberpunkCba
@@ -8,8 +8,8 @@ namespace CyberpunkCba
     static constexpr std::string_view COLOR_RED {"\033[31m"};
     static constexpr std::string_view COLOR_RESET {"\033[0m"};
 
-    UnknownCommand::UnknownCommand(std::string input)
-        : m_input {std::move(input)}
+    UnknownCommand::UnknownCommand(std::string input, const CommandRegistry& registry) //actualizar el constructor para recibir la referencia a la tabla de comandos, y guardarla en el atributo m_registry.
+        : m_input {std::move(input)}, m_registry {registry}
     {
     }
 
@@ -26,6 +26,9 @@ namespace CyberpunkCba
         }
         std::cout << COLOR_RED << "  Comando desconocido: \"" << m_input << "\"\n"
                   << COLOR_RESET << "  Escribí 'help' para ver los comandos disponibles.\n\n";
+        
+        // caso white-only
+        
     }
 
     std::string UnknownCommand::name() const
