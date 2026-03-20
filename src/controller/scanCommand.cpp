33e9750848b01175b3bbe82800605d023bc3ef20
Luis <luis.palacio@mi.unc.edu.ar>
2026-03-20 17:48:05 -0300

    style: format scan command switch block

--- src/controller/scanCommand.cpp
+++ src/controller/scanCommand.cpp
@@ -21,14 +21,14 @@ namespace CyberpunkCba
 
             switch (hostileCount)
             {
-            case 1:
-                return AlertLevel::Low;
-            case 2:
-                return AlertLevel::Medium;
-            case 3:
-                return AlertLevel::High;
-            default:
-                return AlertLevel::None;
+                case 1:
+                    return AlertLevel::Low;
+                case 2:
+                    return AlertLevel::Medium;
+                case 3:
+                    return AlertLevel::High;
+                default:
+                    return AlertLevel::None;
             }
         }
     } // namespace
