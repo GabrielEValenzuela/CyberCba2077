e12888c144a5ec5fb7784a02cad742bb4a07eb0e
Alejo <alejososaluna@gmail.com>
2026-08-25 19:24:29 -0300

    Creacion de Archivos para CheckpointStack

---
+++ include/cybercba/game/CheckpointStack.h
@@ -0,0 +1,12 @@
+//
+// Created by alejo on 25/8/26.
+//
+
+#ifndef CYBERCBA2077_CHECKPOINTSTACK_H
+#define CYBERCBA2077_CHECKPOINTSTACK_H
+
+class CheckpointStack
+{
+};
+
+#endif // CYBERCBA2077_CHECKPOINTSTACK_H
---
+++ src/game/CheckpointStack.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alejo on 25/8/26.
+//
+
+#include "../../include/cybercba/game/CheckpointStack.h"
