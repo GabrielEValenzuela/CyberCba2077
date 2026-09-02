10a35a4630810d5b461cc7f84f11955b87be6dd9
FacuBatallanes <facundo.batallanes@mi.unc.edu.ar>
2026-09-02 20:35:56 -0300

    tercer commit, cambios hechos en el stack, implementación addVecino

--- include/cybercba/combat/GuardSquadMember.cpp
+++ include/cybercba/combat/GuardSquadMember.cpp
@@ -3,3 +3,50 @@
 //
 
 #include "GuardSquadMember.h"
+
+using namespace GuardSquadMember;
+
+
+
+//acá implementé el constructor, los guardias nacen con cero y sin vecinos
+GuardSquadMember(const EstrategiaDeAtaque* primeraEstrategia)
+{
+    EsteNivelAlerta =0
+    EstaEstrategia = primeraEstrategia;
+}
+
+// para agregar un vecino lo inicio acá
+void addVecino(GuardSquadMember* vecino)
+{
+    //este metodo estaba esta en la estructura del profe
+    //el método pushback lo que hace es tomar el puntero y lo coloca en el primer casillero vacio.
+
+    esteVecino.pushBack(vecino);
+
+}
+
+int RecibirAlerta (int nivel, nivel guardia) {
+    if (nivel > nivelGuardia) {
+        this.Guardia = nivel;
+        avisarVecino();
+    }
+    return nivel;
+}
+
+void avisarVecino();
+{
+
+}
+
+
+int RecibirAlerta (int nivel, nivel guardia)
+{
+// 
+    if (nivel > nivelGuardia)
+    {
+        EsteGuardia =nivelEntrada;
+        avisarVecino();
+
+    }
+    return nivel;
+}
--- include/cybercba/combat/GuardSquadMember.h
+++ include/cybercba/combat/GuardSquadMember.h
@@ -2,11 +2,35 @@
 // Created by facundo on 2/9/26.
 //
 
+cybercba::structures::DynamicArray<GuardSquadMember*>
+
 #ifndef ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_GUARDSQUADMEMBER_H
 #define ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_GUARDSQUADMEMBER_H
 
+// Incluimos la lista dinámica de la cátedra para guardar a los vecinos
+#include "cybercba/structures/DynamicArray.hpp"
+#include "cybercba/combat/GuardBehaviorStrategy.hpp"
+
+using namespace cybercba::combat;
+using namespace cybercba::structures;
+
+
 class GuardSquadMember
 {
+    int nivelAlerta; //esta es la info que tiene el guardia
+    DynArray<GuardSquadMember*> vecinos;  // el arreglo tiene los punteros de los guardias
+
+    const EstrategiaDeAtaque* m_estrategia;
+     public:
+    GuardSquadMember(const EstrategiaDeAtaque* primeraEstrategia); //este es el constructor que crea un guardia
+                                                                    //Este ya nacie
+    //entonces estos de acá abajo son las funciones que se vana a ejecutar
+    //pero como es un header nomás nombro las funciones que deberia tener
+    //las implementaciones van en el cpp
+
+    void addVecino(GuardSquadMember* vecino);
+    void recibirAlerta(int nivel );
+
 };
 
 #endif // ALGORITMOS_Y_ESTRUCTURAS_DE_DATOS_GUARDSQUADMEMBER_H
